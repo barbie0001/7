6a13e1759b024868dabedf0ab6b1cb52230c095d
barbie0001 <varenik303@gmail.com>
2023-11-21 21:17:05 +0300

    ghfrnjc 7

---
+++ practos7/Explorer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practos7
+{
+
+    static public class Explorer
+    {
+        static public bool dr = true;
+        static public string path;
+        static public DriveInfo[] drives;
+        static public string[] paths;
+        static public void ReadDrives()
+        {
+            drives = DriveInfo.GetDrives();
+            Menu.options = new string[drives.Length];
+
+            for (int i = 0; i < drives.Length; i++)
+            {
+
+                long freeSpace = drives[i].TotalFreeSpace / 1073741824;
+                long totalSize = drives[i].TotalSize / 1073741824;
+                Menu.options[i] = $"{drives[i].Name} {freeSpace} ГБ свободно {totalSize}";
+            }
+        }
+        static public void ReadDirectory()
+        {
+            Menu.options = Directory.GetFileSystemEntries(path);
+            paths = Menu.options;
+            for (int i = 0; i < Menu.options.Length; i++)
+            {
+                FileInfo cfile = new FileInfo(Menu.options[i]);
+                Menu.options[i] = $"{Menu.options[i].PadRight(40)}{Directory.GetCreationTime(Menu.options[i])}\t\t{cfile.Extension}";
+            }
+        }
+
+    }
+}
---
+++ practos7/Menu.cs
@@ -0,0 +1,70 @@
+﻿using practos7;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practos7
+{
+    public static class Menu
+    {
+        static public string[] options;
+        static public int selectedOption = 0;
+        static public void DisplayMenu()
+        {
+            ConsoleKeyInfo keyInfo;
+
+            do
+            {
+                Console.Clear();
+                for (int i = 0; i < options.Length; i++)
+                {
+                    if (i == selectedOption)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(options[i]);
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.WriteLine(options[i]);
+                    }
+                }
+
+                keyInfo = Console.ReadKey();
+
+                if (keyInfo.Key == ConsoleKey.UpArrow)
+                {
+                    selectedOption = (selectedOption - 1 + options.Length) % options.Length;
+
+                }
+                else if (keyInfo.Key == ConsoleKey.DownArrow)
+                {
+                    selectedOption = (selectedOption + 1) % options.Length;
+                }
+                else if (keyInfo.Key == ConsoleKey.Enter)
+                {
+                    if (Explorer.dr)
+                    {
+                        Explorer.path = Explorer.drives[selectedOption].Name;
+                        Explorer.dr = false;
+                    }
+                    else
+                    {
+                        File.WriteAllText("path.txt", Explorer.paths[selectedOption]);
+                        Explorer.path = @Explorer.paths[selectedOption];
+                    }
+                    Explorer.ReadDirectory();
+
+                }
+                else if (keyInfo.Key == ConsoleKey.Escape)
+                {
+                    Explorer.ReadDrives();
+                }
+
+            } while (true);
+        }
+    }
+}
